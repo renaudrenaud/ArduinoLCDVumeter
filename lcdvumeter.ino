86aa9b6bb1f5b5d840278e51d3f64f3ae04538c7
renaudrenaud <renaud.renaud@gmail.com>
2019-05-19 22:23:54 +0200

    Now it is working with backlight but...
    Vumeter function seems not really ok:
    -when I use power from wall plug there is a lot of noise, I suppose
    -when I use battery no noise, so vumter level is near zero, ok
    - in this case, just the firrst row is moving => I have to test a "gain" function...

--- lcdvumeter.ino
+++ lcdvumeter.ino
@@ -1,9 +1,9 @@
 /*
-modified by varind in 2013
-this code is public domain, enjoy!
- */
+  modified by varind in 2013
+  this code is public domain, enjoy!
+*/
 #include <Wire.h>
-#include <FastLED.h> 
+#include <FastLED.h>
 #define ARRAY_SIZE(A) (sizeof(A) / sizeof((A)[0]))
 
 #include <LiquidCrystal_I2C.h>
@@ -25,8 +25,8 @@ char im[64], data[64];
 char Rim[64], Rdata[64];
 char data_avgs[32];
 float peaks[32];
-int i = 0,val,Rval;
-int x = 0, y=0, z=0;
+int i = 0, val, Rval;
+int x = 0, y = 0, z = 0;
 int load;
 int nPatterns = 9 ;
 int lightPattern = 1;
@@ -38,34 +38,43 @@ unsigned int recv_size = 0;
 unsigned long prev, interval = 1000;
 
 
-LiquidCrystal_I2C lcd(0x3F,16,2);
+LiquidCrystal_I2C lcd(0x3F, 16, 2);
 
 // VU METER CHARACTERS
 byte v1[8] = {
-  B00000,B00000,B00000,B00000,B00000,B00000,B00000,B11111};
+  B00000, B00000, B00000, B00000, B00000, B00000, B00000, B11111
+};
 byte v2[8] = {
-  B00000,B00000,B00000,B00000,B00000,B00000,B11111,B11111};
+  B00000, B00000, B00000, B00000, B00000, B00000, B11111, B11111
+};
 byte v3[8] = {
-  B00000,B00000,B00000,B00000,B00000,B11111,B11111,B11111};
+  B00000, B00000, B00000, B00000, B00000, B11111, B11111, B11111
+};
 byte v4[8] = {
-  B00000,B00000,B00000,B00000,B11111,B11111,B11111,B11111};
+  B00000, B00000, B00000, B00000, B11111, B11111, B11111, B11111
+};
 byte v5[8] = {
-  B00000,B00000,B00000,B11111,B11111,B11111,B11111,B11111};
+  B00000, B00000, B00000, B11111, B11111, B11111, B11111, B11111
+};
 byte v6[8] = {
-  B00000,B00000,B11111,B11111,B11111,B11111,B11111,B11111};
+  B00000, B00000, B11111, B11111, B11111, B11111, B11111, B11111
+};
 byte v7[8] = {
-  B00000,B11111,B11111,B11111,B11111,B11111,B11111,B11111};
+  B00000, B11111, B11111, B11111, B11111, B11111, B11111, B11111
+};
 byte v8[8] = {
-  B11111,B11111,B11111,B11111,B11111,B11111,B11111,B11111};
+  B11111, B11111, B11111, B11111, B11111, B11111, B11111, B11111
+};
 
 
 void setup() {
-    Wire.begin();
-    // DS3231_init(DS3231_INTCN);
-    memset(recv, 0, BUFF_MAX);
-    // rtc.begin();
-     pinMode(buttonPin, INPUT);
-    digitalWrite(buttonPin, HIGH);  // button pin is HIGH, so it drops to 0 if pressed
+  lcd.backlight();
+  Wire.begin();
+  // DS3231_init(DS3231_INTCN);
+  memset(recv, 0, BUFF_MAX);
+  // rtc.begin();
+  pinMode(buttonPin, INPUT);
+  digitalWrite(buttonPin, HIGH);  // button pin is HIGH, so it drops to 0 if pressed
   //setTheTime("305017101012018");     // ssmmhhDDMMYYYY set time once in the given format
   lcd.begin(16, 2);
   lcd.clear();
@@ -77,8 +86,8 @@ void setup() {
   lcd.createChar(6, v6);
   lcd.createChar(7, v7);
   lcd.createChar(8, v8);
- 
- for (i=0;i<80;i++)
+
+  for (i = 0; i < 80; i++)
   {
     for (load = 0; load < i / 5; load++)
     {
@@ -99,48 +108,50 @@ void setup() {
       lcd.write(1);
     }
     lcd.setCursor(0, 0);
-    lcd.print("  Cine-Lights   ");
-    delay(50);
+    lcd.print(" AudioFoLights  ");
+    delay(10);
   }
+
+
   lcd.clear();
   delay(500);
 }
-  
+
 
 
 void vu() {
-//  delay(10);
-  for (i=0; i < 64; i++){    
+  //  delay(10);
+  for (i = 0; i < 64; i++) {
     val = ((analogRead(LCHAN) / 4 ) - 128);  // chose how to interpret the data from analog in
-    data[i] = val;                                      
-    im[i] = 0;   
-    if (channels ==2){
+    data[i] = val;
+    im[i] = 0;
+    if (channels == 2) {
       Rval = ((analogRead(RCHAN) / 4 ) - 128);  // chose how to interpret the data from analog in
-      Rdata[i] = Rval;                                      
-      Rim[i] = 0;   
+      Rdata[i] = Rval;
+      Rim[i] = 0;
     }
   };
 
-  fix_fft(data,im,6,0); // Send the data through fft
-  if (channels == 2){
-    fix_fft(Rdata,Rim,6,0); // Send the data through fft
+  fix_fft(data, im, 6, 0); // Send the data through fft
+  if (channels == 2) {
+    fix_fft(Rdata, Rim, 6, 0); // Send the data through fft
   }
 
   // get the absolute value of the values in the array, so we're only dealing with positive numbers
-  for (i=0; i< 32 ;i++){  
-    data[i] = sqrt(data[i] * data[i] + im[i] * im[i]); 
+  for (i = 0; i < 32 ; i++) {
+    data[i] = sqrt(data[i] * data[i] + im[i] * im[i]);
   }
-  if (channels ==2){
-    for (i=16; i< 32 ;i++){  
-      data[i] = sqrt(Rdata[i-16] * Rdata[i-16] + Rim[i-16] * Rim[i-16]); 
+  if (channels == 2) {
+    for (i = 16; i < 32 ; i++) {
+      data[i] = sqrt(Rdata[i - 16] * Rdata[i - 16] + Rim[i - 16] * Rim[i - 16]);
     }
   }
 
   // todo: average as many or as little dynamically based on yres
-  for (i=0; i<32; i++) {
+  for (i = 0; i < 32; i++) {
     data_avgs[i] = (data[i]);// + data[i*2+1]);// + data[i*3 + 2]);// + data[i*4 + 3]);  // add 3 samples to be averaged, use 4 when yres < 16
-    data_avgs[i] = constrain(data_avgs[i],0,9-gain);  //data samples * range (0-9) = 9
-    data_avgs[i] = map(data_avgs[i], 0, 9-gain, 0, yres);        // remap averaged values
+    data_avgs[i] = constrain(data_avgs[i], 0, 9 - gain); //data samples * range (0-9) = 9
+    data_avgs[i] = map(data_avgs[i], 0, 9 - gain, 0, yres);      // remap averaged values
   }
 
 } // end loop
@@ -152,13 +163,13 @@ void vu() {
 
 
 
-void decay(int decayrate){
-  //// reduce the values of the last peaks by 1 
-  if (decaytest == decayrate){
-    for (x=0; x < 32; x++) {
+void decay(int decayrate) {
+  //// reduce the values of the last peaks by 1
+  if (decaytest == decayrate) {
+    for (x = 0; x < 32; x++) {
       peaks[x] = peaks[x] - 1;  // subtract 1 from each column peaks
       decaytest = 0;
-      
+
     }
   }
   decaytest++;
@@ -169,7 +180,7 @@ void decay(int decayrate){
 
 void Two16_LCD() {
   vu();
-  decay(1); 
+  decay(1);
   lcd.setCursor(0, 0);
   lcd.print("L"); // Channel ID replaces bin #0 due to hum & noise
   lcd.setCursor(0, 1);
@@ -194,118 +205,118 @@ void Two16_LCD() {
     }
     else {
       lcd.write(peaks[y]);
-    
+
     }
   }
 }
 
-void mono(){ 
+void mono() {
   vu();
-   decay(1); 
+  decay(1);
   lcd.setCursor(0, 0);
   lcd.print(" "); // Channel ID replaces bin #0 due to hum & noise
   lcd.setCursor(0, 1);
   lcd.print("M"); // ditto
-  for (x=1; x < 16; x++) {  // repeat for each column of the display horizontal resolution
-    y = data_avgs[x];  // get current column value 
-        z= peaks[x];
-    if (y > z){
-      peaks[x]=y;
+  for (x = 1; x < 16; x++) { // repeat for each column of the display horizontal resolution
+    y = data_avgs[x];  // get current column value
+    z = peaks[x];
+    if (y > z) {
+      peaks[x] = y;
     }
-    y= peaks[x]; 
+    y = peaks[x];
 
-    if (y <= 8){            
-      lcd.setCursor(x,0); // clear first row
+    if (y <= 8) {
+      lcd.setCursor(x, 0); // clear first row
       lcd.print(" ");
-      lcd.setCursor(x,1); // draw second row
-      if (y == 0){
+      lcd.setCursor(x, 1); // draw second row
+      if (y == 0) {
         lcd.print(" "); // save a glyph
       }
       else {
         lcd.write(y);
       }
     }
-    else{
-      lcd.setCursor(x,0);  // draw first row
-      if (y == 9){
+    else {
+      lcd.setCursor(x, 0); // draw first row
+      if (y == 9) {
         lcd.write(32); // RC  was " "
       }
       else {
-        lcd.write(y-8);  // same chars 1-8 as 9-16
+        lcd.write(y - 8); // same chars 1-8 as 9-16
       }
-      lcd.setCursor(x,1);
-      lcd.write(8);  
+      lcd.setCursor(x, 1);
+      lcd.write(8);
     } // end display
   }  // end xres
 }
 
-void stereo8(){
+void stereo8() {
   vu();
-   decay(1); 
-   lcd.setCursor(8, 1);
+  decay(1);
+  lcd.setCursor(8, 1);
   lcd.print("R"); // Channel ID replaces bin #0 due to hum & noise
-  for (x=1; x < 8; x++) {
+  for (x = 1; x < 8; x++) {
     y = data_avgs[x];
-        z= peaks[x];
-    if (y > z){
-      peaks[x]=y;
+    z = peaks[x];
+    if (y > z) {
+      peaks[x] = y;
     }
-    y= peaks[x]; 
+    y = peaks[x];
 
-    if (y <= 8){            
-      lcd.setCursor(x,0); // clear first row
+    if (y <= 8) {
+      lcd.setCursor(x, 0); // clear first row
       lcd.print(" ");
-      lcd.setCursor(x,1); // draw second row
-      if (y == 0){
+      lcd.setCursor(x, 1); // draw second row
+      if (y == 0) {
         lcd.print(" "); // save a glyph
       }
       else {
         lcd.write(y);
       }
     }
-    else{
-      lcd.setCursor(x,0);  // draw first row
-      if (y == 9){
-        lcd.write(32);  
+    else {
+      lcd.setCursor(x, 0); // draw first row
+      if (y == 9) {
+        lcd.write(32);
       }
       else {
-        lcd.write(y-8);  // same chars 1-8 as 9-16
+        lcd.write(y - 8); // same chars 1-8 as 9-16
       }
-      lcd.setCursor(x,1);
-      lcd.write(8);  
+      lcd.setCursor(x, 1);
+      lcd.write(8);
     }
   }
   lcd.setCursor(0, 1);
   lcd.print("L"); // ditto
-  for (x=17; x < 32; x++) {
+  for (x = 17; x < 32; x++) {
     y = data_avgs[x];
-       z= peaks[x];
-    if (y > z){
-      peaks[x]=y;
+    z = peaks[x];
+    if (y > z) {
+      peaks[x] = y;
     }
-    y= peaks[x]; 
+    y = peaks[x];
 
-    if (y <= 8){            
-      lcd.setCursor(x-8,0); // clear first row
+    if (y <= 8) {
+      lcd.setCursor(x - 8, 0); // clear first row
       lcd.print(" ");
-      lcd.setCursor(x-8,1); // draw second row
-      if (y == 0){
+      lcd.setCursor(x - 8, 1); // draw second row
+      if (y == 0) {
         lcd.print(" "); // save a glyph
       }
       else {
         lcd.write(y);
       }
     }
-    else{
-      lcd.setCursor(x-8,0);  // draw first row
-      if (y == 9){
-        lcd.write(32);  
+    else {
+      lcd.setCursor(x - 8, 0); // draw first row
+      if (y == 9) {
+        lcd.write(32);
       }
       else {
-        lcd.write(y-8);  // same chars 1-8 as 9-16
+        lcd.write(y - 8); // same chars 1-8 as 9-16
       }
-      lcd.setCursor(x-8,1);
-      lcd.write(8);  
+      lcd.setCursor(x - 8, 1);
+      lcd.write(8);
     }
   }
 }
@@ -315,44 +326,44 @@ void  bars  ()
 {
   decay(1);
   vu();
-//  lcd.createChar( i,block[i] );
-  #define T_REFRESH    100            // msec bar refresh rate
-  #define T_PEAKHOLD   5*T_REFRESH    // msec peak hold time before return
-  long  lastT=0;
-//  if( millis()<lastT )
-//    return;
-//  lastT += T_REFRESH;    
-  int anL = map( sqrt( analogRead( LCHAN  )*16 ),0,128,0,80 );  // sqrt to have non linear scale (better was log)
-  int anR = map( sqrt( analogRead( RCHAN )*16 ),0,128,0,80 );
-  bar( 0,anL );
-  bar( 1,anR );
+  //  lcd.createChar( i,block[i] );
+#define T_REFRESH    100            // msec bar refresh rate
+#define T_PEAKHOLD   5*T_REFRESH    // msec peak hold time before return
+  long  lastT = 0;
+  //  if( millis()<lastT )
+  //    return;
+  //  lastT += T_REFRESH;
+  int anL = map( sqrt( analogRead( LCHAN  ) * 16 ), 0, 128, 0, 80 ); // sqrt to have non linear scale (better was log)
+  int anR = map( sqrt( analogRead( RCHAN ) * 16 ), 0, 128, 0, 80 );
+  bar( 0, anL );
+  bar( 1, anR );
 }
 
-void  bar  ( int rows,int levs )
+void  bar  ( int rows, int levs )
 {
-  lcd.setCursor( 0,rows );
+  lcd.setCursor( 0, rows );
   lcd.write( rows ? 'R' : 'L' );
-  for( int i=1 ; i<16 ; i++ )
+  for ( int i = 1 ; i < 16 ; i++ )
   {
-    int f=constrain( levs      -i*5,0,5 );
-    int p=constrain( lmax[rows]-i*5,0,6 );
-    if( f )
+    int f = constrain( levs      - i * 5, 0, 5 );
+    int p = constrain( lmax[rows] - i * 5, 0, 6 );
+    if ( f )
       lcd.write(8);
     else
       lcd.write(32);
   }
-  if( levs>lmax[rows] )
+  if ( levs > lmax[rows] )
   {
     lmax[rows] = levs;
-    dly[rows]  = -(T_PEAKHOLD)/T_REFRESH;                // Starting delay value. Negative=peak don't move
+    dly[rows]  = -(T_PEAKHOLD) / T_REFRESH;              // Starting delay value. Negative=peak don't move
   }
   else
   {
-    if( dly[rows]>0 )
-      lmax[rows] -= dly[rows]; 
+    if ( dly[rows] > 0 )
+      lmax[rows] -= dly[rows];
 
-    if( lmax[rows]<0 )
-      lmax[rows]=0;
+    if ( lmax[rows] < 0 )
+      lmax[rows] = 0;
     else
       dly[rows]++;
   }
@@ -360,22 +371,22 @@ void  bar  ( int rows,int levs )
 
 
 
-void stereo16(){
+void stereo16() {
   vu();
-   decay(1); 
+  decay(1);
   lcd.setCursor(0, 0);
   lcd.print(" "); // Channel ID replaces bin #0 due to hum & noise
-  
-  for (x=1; x < 16; x++) {
+
+  for (x = 1; x < 16; x++) {
     y = data_avgs[x];
-         z= peaks[x];
-    if (y > z){
-      peaks[x]=y;
+    z = peaks[x];
+    if (y > z) {
+      peaks[x] = y;
     }
-    y= peaks[x]; 
-    if (x < xres){            
-      lcd.setCursor(x,0); // draw first row
-      if (y == 0){
+    y = peaks[x];
+    if (x < xres) {
+      lcd.setCursor(x, 0); // draw first row
+      if (y == 0) {
         lcd.print(" "); // save a glyph
       }
       else {
@@ -383,18 +394,18 @@ void stereo16(){
       }
     }
   }
-lcd.setCursor(0, 1);
+  lcd.setCursor(0, 1);
   lcd.print(" "); // Channel ID replaces bin #0 due to hum & noise
-  for (x=17; x < 32; x++) {
+  for (x = 17; x < 32; x++) {
     y = data_avgs[x];
-        z= peaks[x];
-    if (y > z){
-      peaks[x]=y;
+    z = peaks[x];
+    if (y > z) {
+      peaks[x] = y;
     }
-    y= peaks[x]; 
-    if (x-16 < xres){            
-      lcd.setCursor(x-16,1); // draw second row
-      if (y == 0){
+    y = peaks[x];
+    if (x - 16 < xres) {
+      lcd.setCursor(x - 16, 1); // draw second row
+      if (y == 0) {
         lcd.print(" "); // save a glyph
       }
       else {
@@ -405,29 +416,29 @@ lcd.setCursor(0, 1);
 }
 
 
-void thirtytwoband(){
-   vu();
-   decay(1); 
-  for (x=0; x < 32; x++) {
+void thirtytwoband() {
+  vu();
+  decay(1);
+  for (x = 0; x < 32; x++) {
     y = data_avgs[x];
 
-        z= peaks[x];
-    if (y > z){
-      peaks[x]=y;
+    z = peaks[x];
+    if (y > z) {
+      peaks[x] = y;
     }
-    y= peaks[x]; 
-    if (x < 16){            
-      lcd.setCursor(x,0); // draw second row
-      if (y == 0){
+    y = peaks[x];
+    if (x < 16) {
+      lcd.setCursor(x, 0); // draw second row
+      if (y == 0) {
         lcd.print(" "); // save a glyph
       }
       else {
         lcd.write(y);
       }
     }
-    else{
-      lcd.setCursor(x-16,1);
-      if (y == 0){
+    else {
+      lcd.setCursor(x - 16, 1);
+      if (y == 0) {
         lcd.print(" ");
       }
       else {
@@ -438,12 +449,12 @@ void thirtytwoband(){
 }
 
 void simple() {
- lcd.setCursor(0,0);
- lcd.print("  Cine-Lights   ");
- lcd.setCursor(0,1);
- lcd.print(" Time: ");
- lcd.print("No clock");
- 
+  lcd.setCursor(0, 0);
+  lcd.print(" AudioFoLights  ");
+  lcd.setCursor(0, 1);
+  lcd.print(" Time: ");
+  lcd.print("No clock");
+
 }
 
 
@@ -457,36 +468,36 @@ void loop() {
   }
   if (lightPattern > nPatterns) lightPattern = 1;
   oldButtonVal = buttonVal;
-  
-switch(lightPattern) {
-     case 1:
-         simple();
-         break;
-     case 2:
-         // timedate();
-         break;
-     case 3:
-         All();
-         break;
-     case 4:
-         bars();
-         break;
-     case 5:
-         mono();
-         break;
-     case 6:
-         stereo8();
-         break;
-     case 7:
-         stereo16();
-         break;
-     case 8:
-         Two16_LCD();    
-         break;
-     case 9:
-         lcd.clear();   
-         break;
-       
+  lightPattern = 4;
+  switch (lightPattern) {
+    case 1:
+      simple();
+      break;
+    case 2:
+      // timedate();
+      break;
+    case 3:
+      All();
+      break;
+    case 4:
+      bars();
+      break;
+    case 5:
+      mono();
+      break;
+    case 6:
+      stereo8();
+      break;
+    case 7:
+      stereo16();
+      break;
+    case 8:
+      Two16_LCD();
+      break;
+    case 9:
+      lcd.clear();
+      break;
+
   }
 }
 
@@ -504,6 +515,8 @@ void All()
 {
   // Call the current pattern function once, updating the 'leds' array
   gPatterns[gCurrentPatternNumber]();
-  EVERY_N_SECONDS( 20 ) { nextPattern(); } // change patterns periodically
+  EVERY_N_SECONDS( 20 ) {
+    nextPattern();  // change patterns periodically
+  }
 }
 // second list
